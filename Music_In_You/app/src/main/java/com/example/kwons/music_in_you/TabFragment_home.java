5e63fe6800c61a599f3f9a08cbb990a0a3911c0a
unknown <shyunju7@naver.com>
2019-08-27 18:03:54 +0900

    add home activity

--- Music_In_You/app/src/main/java/com/example/kwons/music_in_you/TabFragment_home.java
+++ Music_In_You/app/src/main/java/com/example/kwons/music_in_you/TabFragment_home.java
@@ -1,22 +1,34 @@
 package com.example.kwons.music_in_you;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TabFragment_home extends Fragment {
 
     TextView textView;
+    ImageView mic;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         View view = inflater.inflate(R.layout.tab_home,container,false);
 
-        textView = view.findViewById(R.id.tv);
-        textView.setText("Home 화면!!!!!!!!!");
+        textView = view.findViewById(R.id.mic_msg);
+        mic = view.findViewById(R.id.main_mic);
+        mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(),VoiceDetection.class);
+                startActivity(intent);
+            }
+        });
+
 
         return view;
     }
