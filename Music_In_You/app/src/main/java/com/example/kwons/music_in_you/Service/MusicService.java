31c748c36ba2bb6fe12f27271d5bea841cc7bca8
Daeun Lee <eleeje97@gmail.com>
2019-11-03 01:21:22 +0900

    MusicService Test

--- Music_In_You/app/src/main/java/com/example/kwons/music_in_you/Service/MusicService.java
+++ Music_In_You/app/src/main/java/com/example/kwons/music_in_you/Service/MusicService.java
@@ -3,12 +3,17 @@ package com.example.kwons.music_in_you.Service;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.IBinder;
+import android.provider.MediaStore;
+
+import com.example.kwons.music_in_you.MusicDTO;
 
 public class MusicService extends Service {
-    final static String MESSAGE_KEY = "MusicPlay"; // 인텐트로 주고받는 재생 flag
-
+    public final static String MESSAGE_KEY = "MusicPlay"; // 인텐트로 주고받는 재생 flag
+    public final static String MUSIC_DTO = "MusicDTO";// 재생할 음악의 DTO 객체를 받을 인텐트 flag
     MediaPlayer mediaPlayer;
+    Uri musicURI; // 재생할 음악의 URI
 
     public MusicService() {
     }
@@ -16,9 +21,13 @@ public class MusicService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         boolean message = intent.getExtras().getBoolean(MusicService.MESSAGE_KEY);
+        MusicDTO musicDTO = (MusicDTO) intent.getExtras().get(MusicService.MUSIC_DTO);
+
+        musicURI = Uri.withAppendedPath(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, "" + musicDTO.getId());
 
         if(message) {
-            //mediaPlayer = MediaPlayer.create(this, 음악 URI);
+            mediaPlayer = MediaPlayer.create(this, musicURI);
             mediaPlayer.start();
         } else {
             mediaPlayer.stop();
--- Music_In_You/app/src/main/java/com/example/kwons/music_in_you/TabFragment_setting.java
+++ Music_In_You/app/src/main/java/com/example/kwons/music_in_you/TabFragment_setting.java
@@ -1,17 +1,44 @@
 package com.example.kwons.music_in_you;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.kwons.music_in_you.Service.MusicService;
+
+import java.util.ArrayList;
 
 public class TabFragment_setting extends Fragment {
-
+    Button button;
+    ArrayList<MusicDTO> list;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        View view = inflater.inflate(R.layout.tab_setting,container,false);
 
-        return inflater.inflate(R.layout.tab_setting, container, false);
+        list = MainActivity.mainActivity.getMusicList(); // 음악 리스트 가져오기
+
+
+        /** 음악 서비스 테스트 **/
+        button = view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), MusicService.class);
+                intent.putExtra(MusicService.MESSAGE_KEY, true);
+
+                intent.putExtra(MusicService.MUSIC_DTO, list.get(5));
+                getActivity().startService(intent);
+            }
+        });
+
+
+
+        return view;
     }
 }
