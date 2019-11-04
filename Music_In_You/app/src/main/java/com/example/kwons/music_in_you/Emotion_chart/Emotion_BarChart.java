777fd8552b299ad37b70a2c8a8a987c67124a605
unknown <shyunju7@naver.com>
2019-11-05 06:50:10 +0900

    finally modify

--- Music_In_You/app/src/main/java/com/example/kwons/music_in_you/Emotion_chart/Emotion_BarChart.java
+++ Music_In_You/app/src/main/java/com/example/kwons/music_in_you/Emotion_chart/Emotion_BarChart.java
@@ -3,6 +3,7 @@ package com.example.kwons.music_in_you.Emotion_chart;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -21,6 +22,7 @@ import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Emotion_BarChart extends Activity  {
 
@@ -32,6 +34,11 @@ public class Emotion_BarChart extends Activity  {
     // 차트 데이터 셋을 담을 list
     private List<BarEntry> EntryData;
 
+
+    // 글씨체
+    Typeface typeface, typeface2;
+    private final static String PATH_TO_WEATHER_FONT = "fonts/font1.otf";
+    private final static String PATH_TO_WEATHER_FONT2 = "fonts/title_font.otf";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,20 +50,78 @@ public class Emotion_BarChart extends Activity  {
         // 전달받은 라벨
         String label = intent.getExtras().getString("emotion");
 
+        // 텍스트
+        TextView emotion_ment = findViewById(R.id.emotion_commend);
+        TextView barChart_name = findViewById(R.id.barChart_name);
+        barChart_name.setTypeface(typeface2);
+        // 감정별 멘트 저장
+        String[] happy_ment = {"즐거운 한달 보내셨나요?\n" +
+                "앞으로도 이번달처럼 행복한 일 가득하길 MIYU가 응원할게요!",
+                               "웃으면 복이 온대요! 오늘부터 1일 30웃음 도전!!\n" +
+                                       "MIYU는 1일 100웃음 한답니다! 하하하^__^",
+                               "행복은 사소한 것에서부터 시작된답니다!\n" +
+                                       "지금 당장 작은 일부터 시작해보는 것이 어떨까요!\n" +
+                                       "MIYU는 언제나 당신을 응원합니다!",
+                               "당신을 빛나게 하는 가장 쉬운 방법 알고 계시나요?\n" +
+                                       "그것은 바로! ^__________^ 스마일!",
+                               "얼굴 찌푸리지 말아요~ 모두가 힘들잖아요!\n" +
+                                       "기쁨의 그 날 위해 함께 할 MIYU가 있잖아요!♡"};
+        String[] sad_ment = {"적당한 슬픈은 우리에게 유익하답니다.\n" +
+                "지금 이슬픔에 힘들어하지 말고 즐기세요!",
+                "지친 마음과 외로움이 가득 한가요?\n" +
+                "MIYU가 당신의 지친 마음을 달래드릴게요!\n" +
+                "그리고 언제나 옆에 있을게요!",
+                "지금 힘든 마음 때문에 너무 슬퍼하지 마세요!\n" +
+                        "당신에겐 더욱 더 나은 미래가 있답니다!",
+                "곁에 아무도 없다는 생각이 들 때, 주위를 둘러보세요!\n" +
+                        "당신의 곁에는 당신을 아껴주고 사랑해주는 사람들로 \n" +
+                        "가득할거에요! MIYU도 당신을 응원한답니다!^0^",
+                "한번쯤은 '나'만 생각하는 이기심도 필요하답니다!\n" +
+                        "내인생에 가장 소중한 존재는 당신이니까요!",
+                            "지나간 일에 후회하고 슬퍼하는 것보다\n" +
+                                    "앞으로의 '나'를 위한 시간을 갖는 것은 어떨까요!\n" +
+                                    "MIYU는 그런 당신을 응원한답니다!^0^"};
+        String[] angry_ment = {"화가 나는 것이 당연하고, 그런 상황에서 눈물 나는 것이 당연한 거에요! 당신이 약해서 그런 것이 아니랍니다.",
+                "다른 사람의 어리석은 행동에 화를 내어 당신의 마음을 아프게 하지 마세요!" +
+                        "당신이 아프면 MIYU도 아프답니다.",
+                "기분 나쁘고 화나는 일은 우리 같이 훌훌 털어버리고!\n" +
+                        "더 좋은 일, 행복한 일만 만들어 봐요!^ㅇ^",
+                "분노를 가라앉히는 좋은 방법은 아무것도 하지 않고 가만히 있는 것이랍니다! \n" +
+                        "MIYU와 함께 노래를 들으며 힐링타임을 가져볼까요?",
+                "짜증을 내어서 무엇하나, 성화를 바치어 무엇하나\n" +
+                "속상한 일도 하도 많으니 놀기도 하면서 살아가세\n" +
+                "        \t\t\t- 태평가 일부-",
+                "사람에게 쓸모없는 감정은 없습니다. 단지 조절해야할 감정이 있을 뿐이죠! MIYU와 함께 음악을 들으며 감정을 다스려보아요!"
+        };
+        String[] calm_ment = {"세상에서 가장 현명한 사람은 감정을 조절할 수 있는 사람이래요! " +
+                "바로 당신이 그 현명한 사람이 아닐까요?",
+                "","","",""};
+
+
+
+        // 랜덤
+        Random random = new Random();
+        int emotion_data =0;
+
+        // 글씨체
+        typeface = Typeface.createFromAsset(getAssets(), PATH_TO_WEATHER_FONT);
+        typeface2 = Typeface.createFromAsset(getAssets(), PATH_TO_WEATHER_FONT2);
+        // 막대 차트
         mChart = (BarChart) findViewById(R.id.barchart);
         mChart.setDrawBarShadow(false);
         mChart.getDescription().setEnabled(false);
         mChart.setPinchZoom(false);
         mChart.setDrawGridBackground(false);
         // empty labels so that the names are spread evenly
-        String[] labels = { "","1Week", "2Week", "3Week", "4Week", "5Week",""};
+        String[] labels = { "","1Week", "2Week", "3Week", "4Week",""};
         IAxisValueFormatter xAxisFormatter = new LabelFormatter(mChart, labels);
         XAxis xAxis = mChart.getXAxis();
         //xAxis.setCenterAxisLabels(true);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setDrawGridLines(false);
+        xAxis.setDrawGridLines(true);
         xAxis.setGranularity(1f); // only intervals of 1 day
         xAxis.setTextColor(Color.WHITE);
+        xAxis.setTypeface(typeface2);
         xAxis.setTextSize(12);
         xAxis.setAxisLineColor(Color.WHITE);
         xAxis.setAxisMinimum(0f);
@@ -68,7 +133,7 @@ public class Emotion_BarChart extends Activity  {
         leftAxis.setTextColor(Color.WHITE);
         leftAxis.setTextSize(12);
         leftAxis.setAxisLineColor(Color.WHITE);
-        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawGridLines(true);
         leftAxis.setGranularity(2);
         leftAxis.setLabelCount(5, true);
         leftAxis.setAxisMinimum(0f); // Y축 최소 0부터 시작
@@ -77,7 +142,7 @@ public class Emotion_BarChart extends Activity  {
         mChart.getAxisRight().setEnabled(false);
         mChart.getLegend().setEnabled(true);
 
-        float[] valOne = {0, 3, 5, 2, 8, 1, 0};
+        float[] valOne = {0, 3, 5, 2, 8, 0};
 
 
 
@@ -97,20 +162,33 @@ public class Emotion_BarChart extends Activity  {
 
         // 감정별로 그래프 색과 라벨 지정하기
         switch (label){
+
             case "Happy":
+                emotion_data = random.nextInt(happy_ment.length);
                 barDataSet.setColor(Color.parseColor(colors[0])); // Happy일 때 색상
+                emotion_ment.setText(happy_ment[emotion_data]);
+                emotion_ment.setTypeface(typeface);
                 break;
 
             case "Angry":
+                emotion_data = random.nextInt(angry_ment.length);
                 barDataSet.setColor(Color.parseColor(colors[1])); // Angry일 때 색상
+                emotion_ment.setText(angry_ment[emotion_data]);
+                emotion_ment.setTypeface(typeface);
                 break;
 
             case "Sad":
+                emotion_data = random.nextInt(sad_ment.length);
                 barDataSet.setColor(Color.parseColor(colors[2])); // Sad일 때 색상
+                emotion_ment.setText(sad_ment[emotion_data]);
+                emotion_ment.setTypeface(typeface);
                 break;
 
             case "Calm":
+                emotion_data = random.nextInt(sad_ment.length);
                 barDataSet.setColor(Color.parseColor(colors[3])); // Calm일 때 색상
+                emotion_ment.setText(calm_ment[emotion_data]);
+                emotion_ment.setTypeface(typeface);
                 break;
         }
 
@@ -128,6 +206,13 @@ public class Emotion_BarChart extends Activity  {
         mChart.setScaleEnabled(false);
         mChart.setVisibleXRangeMaximum(4f);
         mChart.invalidate();
+
+
+
+
+        // 감정별 멘트
+
+
 
     }
 
---
+++ Music_In_You/app/src/main/java/com/example/kwons/music_in_you/Weather_result_Test.java
@@ -0,0 +1,77 @@
+package com.example.kwons.music_in_you;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class Weather_result_Test extends Activity {
+
+
+    // 리스트
+    public static ArrayList<MusicDTO> list; // 모든 노래 리스트
+    public ArrayList<MusicDTO> musicList; // 특정 재생목록의 노래 리스트
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+
+
+        Button retry_btn, play_btn;
+        retry_btn = findViewById(R.id.retry);
+        play_btn = findViewById(R.id.direct_play);
+        ImageView album = findViewById(R.id.imageView3); // 노래 앨범 담을 이미지뷰
+
+        // 비트맵
+        Bitmap bitmap; // 앨범 사진 가져올 비트맵
+        BitmapDrawable bitmapDrawable; // 비트맵을 Drawable로 바꿔줄 변수
+
+
+
+        // 다시 추천 하기 버튼을 눌렀을 때
+        retry_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        // 즉시 재생 버튼을 눌렀을 때
+        play_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        list = MainActivity.mainActivity.getMusicList(); // 음악 리스트 가져오기
+
+
+        // 이미지 뷰
+        bitmap = BitmapFactory.decodeFile(MusicPlayActivity.getCoverArtPath(
+                Long.parseLong(list.get(0).getAlbumId()),getApplicationContext()));
+        if(bitmap != null) {
+            bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
+          //  all_songs.setBackground(bitmapDrawable);
+          //  all_songs_count.setText(list.size() + all_songs_count.getText().toString());
+        }
+        else{
+           // all_songs.setBackground(getActivity().getDrawable(R.drawable.default_music_album));
+           // all_songs_count.setText(list.size() + all_songs_count.getText().toString());
+        }
+
+
+
+
+    }
+}
